b2dc3e20ccef74241e80fed63435a1c9bf92baaa
thandazilegcisa <gcisatahn@gmail.com>
2021-10-12 05:02:18 +0200

    Blogs Done

--- Gamified Survey/survey.js
+++ Gamified Survey/survey.js
@@ -1,29 +1,53 @@
 let character = document.getElementById("character");
-let block = document.getElementById("block");
-
+let optionOne = document.getElementById("option-One").getBoundingClientRect(); 
+let optionTwo = document.getElementById("option-Two")
+let optionThree = document.getElementById("option-Three")
 
 document.addEventListener('keydown' , function(event){
     if(event.code == "Space"){
-     block.classList.add("animate");
+     character.classList.add("animate");
 
-     console.log("You hit me")
     }
 })
 document.addEventListener("keyup", function(event){
     if(event.code == "Space"){
         setTimeout(function(){
-            block.classList.remove("animate");
+            character .classList.remove("animate");
         })
     }    
 })
 
+
+function touching(div1,div2){
+    let ox = Math.abs(character.x - optionOne.x) < (character.x < optionOne.x ? optionOne.width : character.width);
+    let oy = Math.abs(character.y - optionOne.y) < (character.y < optionOne.y ? optionOne.height : character.height);
+    return ox && oy;
+}
+
+var t = touching(character,optionOne)
+/*
+let collsionCheck = setInterval(function(){
+    var characterTop = window.getComputedStyle(character).getPropertyValue("top")
+    var optionOneBottom = window.getComputedStyle(optionOne).getPropertyValue("bottom");
+    var optionTwoBottom = window.getComputedStyle(optionTwo).getPropertyValue("bottom");
+    var optionThreeBottom = window.getComputedStyle(optionThree).getPropertyValue("bottom");
+
+    if(optionOneBottom === 144){
+        console.log("Hello");
+        alert("Hit");
+    }
+    if(optionTwoBottom == characterTop){
+        console.log("Hello");
+        alert("Hit");
+    }
+    if(optionThreeBottom == characterTop){
+        console.log("Hello");
+        alert("Hit");
+    }
+},10); 
+
+ 
 document.addEventListener("click", function(event){
-    console.log(`xAxis: ${event.offsetX} yAxis ${event.offsetY}`)
+    console.log(`xAxis: ${event.offsetX} yAxis ${event.offsetY}`)   
 })
-
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-
-canvas.width = 920
-canvas.height = 600
-
+*/
